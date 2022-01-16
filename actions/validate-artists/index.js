c3c8085ea5b424062064523d671cfe6d75cd2012
Aleksandr Elmekeev <donamok@gmail.com>
2022-01-16 22:59:13 +0000

    First version of validate artists check

---
+++ actions/validate-artists/index.js
@@ -0,0 +1,25 @@
+const core = require('@actions/core')
+const fs = require('fs')
+
+const count = names =>
+  names.reduce((a, b) => ({ ...a,
+    [b]: (a[b] || 0) + 1
+  }), {})
+
+const duplicates = dict =>
+  Object.keys(dict).filter((a) => dict[a] > 1)
+
+try {
+  let root = process.env.GITHUB_WORKSPACE
+
+  const data = fs.readFileSync(`${root}/playlist.json`, 'utf8')
+  const playlist = JSON.parse(data)
+
+  const allArtists = playlist.map(t => t.artist.split(',')).flat()
+  const duplicatedArtists = duplicates(count(allArtists))
+  if (duplicatedArtists.length > 0) {
+    throw `The following artists are duplicated in the playlist ${duplicatedArtists}`
+  }
+} catch (error) {
+  core.setFailed(error.message)
+}
