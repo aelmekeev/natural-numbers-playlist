f995005916a8236e9eaa0ac2c6cc16c919923910
Alex Elmekeev <55494848+aelmekeev@users.noreply.github.com>
2022-01-16 23:37:10 +0000

    Add artists uniqueness validation (#1)

---
+++ validate/validate-artists.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+const count = names =>
+  names.reduce((a, b) => ({ ...a,
+    [b]: (a[b] || 0) + 1
+  }), {})
+
+const duplicates = dict =>
+  Object.keys(dict).filter((a) => dict[a] > 1)
+
+let root = process.env.GITHUB_WORKSPACE
+
+const data = fs.readFileSync(`${root}/playlist.json`, 'utf8')
+const playlist = JSON.parse(data)
+
+const allArtists = playlist.map(t => t.artist.split(',')).flat()
+const duplicatedArtists = duplicates(count(allArtists))
+if (duplicatedArtists.length > 0) {
+  throw new Error(`The following artists are duplicated in the playlist: ${duplicatedArtists.join(', ')}`)
+}
