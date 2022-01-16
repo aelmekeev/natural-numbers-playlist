01ea8f26c3733c372c09c4306ffd794faf578d95
Aleksandr Elmekeev <donamok@gmail.com>
2022-01-16 23:34:18 +0000

    Remove try-catch

--- validate/validate-artists.js
+++ validate/validate-artists.js
@@ -8,17 +8,13 @@ const count = names =>
 const duplicates = dict =>
   Object.keys(dict).filter((a) => dict[a] > 1)
 
-try {
-  let root = process.env.GITHUB_WORKSPACE
+let root = process.env.GITHUB_WORKSPACE
 
-  const data = fs.readFileSync(`${root}/playlist.json`, 'utf8')
-  const playlist = JSON.parse(data)
+const data = fs.readFileSync(`${root}/playlist.json`, 'utf8')
+const playlist = JSON.parse(data)
 
-  const allArtists = playlist.map(t => t.artist.split(',')).flat()
-  const duplicatedArtists = duplicates(count(allArtists))
-  if (duplicatedArtists.length > 0) {
-    throw new Error(`The following artists are duplicated in the playlist: ${duplicatedArtists.join(', ')}`)
-  }
-} catch (error) {
-  console.error(error.message)
+const allArtists = playlist.map(t => t.artist.split(',')).flat()
+const duplicatedArtists = duplicates(count(allArtists))
+if (duplicatedArtists.length > 0) {
+  throw new Error(`The following artists are duplicated in the playlist: ${duplicatedArtists.join(', ')}`)
 }
