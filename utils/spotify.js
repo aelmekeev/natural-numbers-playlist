72dbe6f1de5839d425840270b8d3a895ee6cb3a4
Alex Elmekeev <55494848+aelmekeev@users.noreply.github.com>
2022-01-17 01:44:45 +0000

    Add spotify metadata check (#3)

---
+++ utils/spotify.js
@@ -0,0 +1,41 @@
+const https = require('https')
+
+const baseOptions = token => ({
+  hostname: 'api.spotify.com',
+  method: 'GET',
+  headers: {
+    'Authorization': `Bearer ${token}`,
+    'Accept': 'application/json'
+  }
+})
+
+const getTracks = (token, trackIds, callback) => {
+  const req = https.request({
+    ...baseOptions(token),
+    path: `/v1/tracks?ids=${trackIds.join(',')}`
+  }, res => {
+    let json = ''
+
+    res.on('data', chunk => {
+      json += chunk
+    })
+
+    res.on('end', () => {
+      if (res.statusCode === 200) {
+        let data = JSON.parse(json)
+        callback(trackIds, data.tracks)
+      } else {
+        console.error('Error. Status: ', res.statusCode)
+      }
+    })
+  })
+
+  req.on('error', error => {
+    // do nothing
+  })
+  req.end()
+}
+
+module.exports = {
+  getTracks
+}
---
+++ validate/validate-metadata.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const spotifyAuth = require('../utils/auth')
+const spotify = require('../utils/spotify')
+
+let root = process.env.GITHUB_WORKSPACE
+
+const data = fs.readFileSync(`${root}/playlist.json`, 'utf8')
+const playlist = JSON.parse(data)
+
+const validateMetadata = (trackIds, tracks) => {
+  tracks = tracks.filter(t => t != null)
+  if (trackIds.length != tracks.length) {
+    const missingTracksIds = trackIds.filter(tId => !tracks.map(t => t.id).includes(tId));
+    throw new Error(`Not all the tracks can be found in Spotify. Missing tracks ids: ${missingTracksIds.join(', ')}`)
+  }
+
+  let batchHasErrors = false;
+  for (const trackId of trackIds) {
+    const localTrack = playlist.find(t => t.id == trackId)
+    const spotifyTrack = tracks.find(t => t.id == trackId)
+    const spotifyTrackArtists = spotifyTrack.artists.reduce((acc, cur) => acc + cur.name, '')
+    if (localTrack.name != spotifyTrack.name || localTrack.artist != spotifyTrackArtists) {
+      batchHasErrors = true
+      console.log(`For song 'https://open.spotify.com/track/${trackId}' name per Spotify is '${spotifyTrack.name}' and artists are '${spotifyTrackArtists}'`)
+    }
+  }
+
+  if (batchHasErrors) {
+    throw new Error('Spotify metadata is not in sync. Please check errors above.')
+  }
+}
+
+
+const validate = (token) => {
+  for (let i = 0; i < Math.ceil(playlist.length / 50); i++) {
+    spotify.getTracks(token, playlist.map(t => t.id).slice(50 * i, 50 * (i + 1)), validateMetadata)
+  }
+}
+
+spotifyAuth.auth(validate)
