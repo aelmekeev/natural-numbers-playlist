8ee8cc00c9ee80fc2ef714b1977194e2fb0239c7
Aleksandr Elmekeev <donamok@gmail.com>
2022-01-18 22:56:41 +0000

    Remove try-catch

--- utils/sync.js
+++ utils/sync.js
@@ -5,7 +5,6 @@ const root = process.env.GITHUB_WORKSPACE
 const authFile = `${root}/auth.json`
 
 const PLAYLIST_ID = '3aMXF1tA7L1ml1XrEqpa0s'
-let AUTH_TOKEN = ''
 
 const add = () => {
   const data = fs.readFileSync(`${root}/playlist.json`, 'utf8')
@@ -18,10 +17,6 @@ const clear = tracks => {
   spotify.deleteTracksFromPlaylist(AUTH_TOKEN, PLAYLIST_ID, tracks.map(t => t.track.id), add)
 }
 
-try {
-  const auth = fs.readFileSync(authFile, 'utf8')
-  AUTH_TOKEN = JSON.parse(auth).access_token
-  spotify.getPlaylistTracks(AUTH_TOKEN, PLAYLIST_ID, clear)
-} catch {
-  throw new Error('Unable to read the cached auth.json.')
-}
+const auth = fs.readFileSync(authFile, 'utf8')
+const AUTH_TOKEN = JSON.parse(auth).access_token
+spotify.getPlaylistTracks(AUTH_TOKEN, PLAYLIST_ID, clear)
