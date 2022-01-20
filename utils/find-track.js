64d2efb79196ada4214d55bbbc9ade53124ade57
Alex Elmekeev <55494848+aelmekeev@users.noreply.github.com>
2022-01-20 23:57:59 +0000

    Add find track script (#13)

---
+++ utils/find-track.js
@@ -0,0 +1,68 @@
+/**
+ * This script allows to find tracks for a specific position.
+ * Usage: SPOTIFY_CLIENT_ID=<client id> SPOTIFY_CLIENT_SECRET=<client secret> node ./utils/find-track.js <number to find track for>
+ */
+const config = require('./config')
+const spotifyAuth = require('./auth')
+const spotify = require('./spotify')
+const utils = require('./utils')
+
+let AUTH_TOKEN = ''
+let tracks = []
+
+const number = process.argv[2]
+const numberAsText = utils.convertNumberToWords(number)
+const dehydratedNumberAsText = utils.dehydrateTitle(numberAsText)
+
+const dedupe = tracks => {
+  const uniqueArtists = [...new Set(tracks.map(t => t.artists))];
+
+  let dedupedTracks = []
+  for (const uniqueArtist of uniqueArtists) {
+    dedupedTracks.push(tracks.find(t => t.artists == uniqueArtist))
+  }
+
+  return dedupedTracks
+}
+
+const analyseResults = foundTracks => {
+  tracks.push(...foundTracks)
+  const filteredTracks = dedupe(tracks
+    .map(t => ({
+      id: t.id,
+      name: t.name,
+      popularity: t.popularity,
+      artists: t.artists.map(a => a.id).join(',')
+    }))
+    .filter(t => t.popularity > 15)
+    .filter(t => [number, dehydratedNumberAsText].includes(utils.dehydrateTitle(t.name)))
+    .sort((a, b) => b.popularity - a.popularity)
+  )
+
+  spotify.getArtists(AUTH_TOKEN, filteredTracks.flatMap(t => t.artists.split(',')), artists => {
+    const popularArtists = artists.filter(a => a.popularity > config.popularityThreshold).map(a => a.id)
+
+    if (popularArtists.length) {
+      console.log('The following tracks were found:')
+    }
+    
+    filteredTracks.map(track => {
+      if (track.artists.split(',').every(a => popularArtists.includes(a))) {
+        console.log(JSON.stringify(utils.dehydrateTrack(tracks.find(t => t.id == track.id)), null, 2))
+        console.log(`Link to track: https://open.spotify.com/track/${track.id}`)
+      }
+    })
+  })
+}
+
+const searchForTracksWordVersion = foundTracks => {
+  tracks.push(...foundTracks)
+  spotify.searchForTracks(AUTH_TOKEN, numberAsText, analyseResults)
+}
+
+const searchForTracks = token => {
+  AUTH_TOKEN = token
+  spotify.searchForTracks(AUTH_TOKEN, number, searchForTracksWordVersion)
+}
+
+spotifyAuth.auth(searchForTracks)
---
+++ utils/utils.js
@@ -0,0 +1,42 @@
+// convert number to words
+
+const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+const convert_hundreds = num => {
+  if (num > 99) {
+    return ones[Math.floor(num / 100)] + ' hundred ' + convert_tens(num % 100)
+  } else {
+    return convert_tens(num)
+  }
+}
+
+const convert_tens = num => {
+  if (num < 10) return ones[num]
+  else if (num >= 10 && num < 20) return teens[num - 10]
+  else {
+    return tens[Math.floor(num / 10)] + ' ' + ones[num % 10]
+  }
+}
+
+const convertNumberToWords = num => convert_hundreds(num).trim()
+
+// end conversion code
+
+const dehydrateTitle = title => title
+  .toLowerCase()
+  .replace(/remastered|2\d{3}|19\d{2}/g, '')  // add any strings agreed to ignore here
+  .replace(/\W/g, '')
+
+const dehydrateTrack = track => ({
+  id: track.id,
+  name: track.name,
+  artist: track.artists.map(a => a.name).join(', ')
+})
+
+module.exports = {
+  convertNumberToWords,
+  dehydrateTitle,
+  dehydrateTrack
+}
